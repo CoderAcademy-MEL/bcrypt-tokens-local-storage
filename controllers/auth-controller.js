dcf461d5e46a88a4d97716313622d651c91745a8
Harrison Malone <harrison.malone2@gmail.com>
2019-06-21 16:30:15 +1000

    added code for creating new user and token

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,4 +1,6 @@
 const jwt = require('jsonwebtoken');
+const mongoose = require('mongoose');
+const User = require('../models/User')
 
 const generateToken = (user) => {
   return jwt.sign(user, process.env.JWT_SECRET);
@@ -7,12 +9,35 @@ const generateToken = (user) => {
 // auth post endpoint goes here
 const register = (req, res) => {
   // object deconstruction to access username and password
+  const { username, password, role } = req.body
   // search the database for the user with mongoose method like .findOne(), make sure the username doesn't already exist  
-  // if the username does exist send an error response
-  // create the new user using mongoose, method like .create()
-  // req.body is new user
-  const token = generateToken(req.body)
-  return res.send({token: token})
+  if (username && password) {
+    User.findOne({ username })
+      .then((doc) => {
+        if (doc) {
+          // if the username does exist send an error response
+          res.status(400).send('incorrect credentials')
+        } else {
+          const newUser = new User({
+            name: username,
+            password: password,
+            role: role
+          })
+          newUser.save((err) => {
+            if (err) return res.status(400).send('incorrect credentials entered')
+          })
+          // create the new user using mongoose, method like .create()
+          // req.body is new user
+          const token = generateToken(req.body)
+          return res.send({token: token})
+        }
+      })
+      .catch((err) => {
+        res.status(400).send('incorrect credentials')
+      })
+  } else {
+    res.status(400).send('incorrect credentials')
+  }
 }
 
 module.exports = {
--- models/User.js
+++ models/User.js
@@ -1,1 +1,13 @@
 // user schema
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  user: String,
+  password: String,
+  role: String
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User
